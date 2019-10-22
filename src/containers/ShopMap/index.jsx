401825cc5071ccadcebadc04d970eba6f941f1b7
remillary <konstantin@muskrat.dev>
2019-10-22 21:58:21 +0700

    fixes

--- src/containers/ShopMap/index.jsx
+++ src/containers/ShopMap/index.jsx
@@ -5,9 +5,11 @@ class ShopMap extends Component {
     componentDidMount() {
         const {H} = window
         const {map, geo} = H
+
         const startLat = 3.146727
         const startLng = 101.693620
         const startZoom = 12
+
         const polygonStyle = {
             fillColor: 'rgba(0, 0, 100, .5)',
             lineWidth: 0
@@ -22,29 +24,23 @@ class ShopMap extends Component {
             [3.147929, 101.668939, 0]
         ]
 
-        const svgCircle = '<svg width="20" height="20" version="1.1" xmlns="http://www.w3.org/2000/svg">' +
-            '<circle cx="10" cy="10" r="7" fill="transparent" stroke="red" stroke-width="4"/>' +
-            '</svg>'
+        const svgCircle = '<svg width="20" height="20" version="1.1" xmlns="http://www.w3.org/2000/svg">'
+            + '<circle cx="10" cy="10" r="7" fill="transparent" stroke="red" stroke-width="4"/>'
+            + '</svg>'
 
+        const verticeGroup = new map.Group({ visibility: true })
         const polygon = new map.Polygon(
             new geo.Polygon(new geo.LineString(points.flat(1))),
             {style: polygonStyle}
         )
-
-        const verticeGroup = new map.Group({ visibility: false })
-        const mainGroup = new map.Group({
-            volatility: true,
-            objects: [polygon, verticeGroup]
-        })
-        let polygonTimeout
-
         polygon.draggable = true
-
         polygon.getGeometry().getExterior().eachLatLngAlt(function(lat, lng, alt, index) {
             const vertice = new map.Marker(
                 {lat, lng},
                 {
-                    icon: new map.Icon(svgCircle, {anchor: {x: 10, y: 10}})
+                    icon: new map.Icon(svgCircle, {
+                        anchor: {x: 10, y: 10}
+                    })
                 }
             )
             vertice.draggable = true
@@ -52,11 +48,16 @@ class ShopMap extends Component {
             verticeGroup.addObject(vertice)
         })
 
-        this.platform = new H.service.Platform({
+        const mainGroup = new map.Group({
+            volatility: true,
+            objects: [polygon, verticeGroup]
+        })
+
+        const platform = new H.service.Platform({
             app_id: 'PwhAzeVFHuSMGdcjtFvQ',
             app_code: 'yE6QWws10hfiJKPyLE-hIQ'
         })
-        const layer = this.platform.createDefaultLayers()
+        const layer = platform.createDefaultLayers()
 
         const shopMap = new H.Map(
             document.getElementById('map'),
@@ -76,37 +77,10 @@ class ShopMap extends Component {
         // H.ui.UI.createDefault(map, layer, 'en-US')
         shopMap.addObject(mainGroup)
 
-        mainGroup.addEventListener('pointerenter', function(evt) {
-            if (polygonTimeout) {
-                clearTimeout(polygonTimeout)
-                polygonTimeout = null
-            }
-
-            // show vertice markers
-            verticeGroup.setVisibility(true)
-        }, true)
-        mainGroup.addEventListener('pointerleave', function(evt) {
-            var timeout = 5000
-
-            // hide vertice markers
-            polygonTimeout = setTimeout(function() {
-                console.log('hide')
-                verticeGroup.setVisibility(false)
-            }, timeout)
-        }, true)
-
-        verticeGroup.addEventListener('pointerenter', function(evt) {
-            document.body.style.cursor = 'pointer'
-        }, true)
-
-        // event listener for vertice markers group to change the cursor to default
-        verticeGroup.addEventListener('pointerleave', function(evt) {
-            document.body.style.cursor = 'default'
-        }, true)
         verticeGroup.addEventListener('drag', function(evt) {
-            var pointer = evt.currentPointer,
-                geoLineString = polygon.getGeometry().getExterior(),
-                geoPoint = shopMap.screenToGeo(pointer.viewportX, pointer.viewportY)
+            const pointer = evt.currentPointer
+            const geoLineString = polygon.getGeometry().getExterior()
+            const geoPoint = shopMap.screenToGeo(pointer.viewportX, pointer.viewportY)
 
             // set new position for vertice marker
             evt.target.setGeometry(geoPoint)
@@ -114,6 +88,7 @@ class ShopMap extends Component {
             // set new position for polygon's vertice
             geoLineString.removePoint(evt.target.getData()['verticeIndex'])
             geoLineString.insertPoint(evt.target.getData()['verticeIndex'], geoPoint)
+
             polygon.setGeometry(new H.geo.Polygon(geoLineString))
 
             // stop propagating the drag event, so the map doesn't move
