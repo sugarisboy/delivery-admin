fa899698b6ab9cf417a6b81ce34ac07cdc661f72
remillary <konstantin@muskrat.dev>
2019-10-27 21:18:39 +0700

    translated statuses

--- src/containers/order/OrderStatusInfo/OrderStatusInfo.jsx
+++ src/containers/order/OrderStatusInfo/OrderStatusInfo.jsx
@@ -3,15 +3,15 @@ import React from "react";
 export default function OrderStatusInfo(props) {
     switch (props.status) {
         case 0:
-            return<div style={{ color: 'steelBlue', fontWeight: 'bold' }}>Ожидание оплаты</div>
+            return<div style={{ color: 'steelBlue', fontWeight: 'bold' }}>Waiting for payment</div>
         case 1:
-            return<div style={{ color: 'orange', fontWeight: 'bold' }}>Оплачено</div>
+            return<div style={{ color: 'orange', fontWeight: 'bold' }}>Paid</div>
         case 2:
-            return<div style={{ color: 'darkOrange', fontWeight: 'bold' }}>Заказ собирается</div>
+            return<div style={{ color: 'darkOrange', fontWeight: 'bold' }}>In work</div>
         case 3:
-            return<div style={{ color: 'orangeRed', fontWeight: 'bold' }}>Доставка</div>
+            return<div style={{ color: 'orangeRed', fontWeight: 'bold' }}>Delivery</div>
         case 10:
-            return<div style={{ color: 'green', fontWeight: 'bold' }}>Выполнено</div>
+            return<div style={{ color: 'green', fontWeight: 'bold' }}>Done</div>
         case 11:
             return<div style={{ color: 'gray', fontWeight: 'bold' }}>Canceled by user</div>
         case 12:
@@ -21,6 +21,6 @@ export default function OrderStatusInfo(props) {
         case 15:
             return<div style={{ color: 'gray', fontWeight: 'bold' }}>Canceled</div>
         default:
-            return <div>{this.props.status}</div>
+            return <div>Processing</div>
     }
 }
